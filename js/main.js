a147b38397bedce48c6a199c0c713994a01c0abf
Goddemi <abraxas1225@naver.com>
2022-02-26 20:47:16 +0900

    js nav_bar scrolling

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 
 
 
-// nav bar - cursor on , box border //
+// nav bar -  scrolling //
 
+const navBar = document.querySelector(".nav_bar");
+const navBarHeight = navBar.getBoundingClientRect().height;
 
-
+document.addEventListener("scroll", () => {
+if (window.scrollY > navBarHeight) {
+    navBar.classList.add("nav_bar-dark");
+} else {
+    navBar.classList.remove("nav_bar-dark")
+}
+})
