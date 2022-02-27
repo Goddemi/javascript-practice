4a66c00328530fddddfd2e98d2c0014952b6c34f
Goddemi <abraxas1225@naver.com>
2022-02-27 17:24:32 +0900

    js scrolling home opacity

--- js/main.js
+++ js/main.js
@@ -14,9 +14,7 @@ if (window.scrollY > navBarHeight) {
 }
 })
 
-900
 
-console.log(window.scrollY);
 
 
 // nav bar - menu click and moving //
@@ -24,18 +22,52 @@ console.log(window.scrollY);
 const navElement = document.querySelector(".bar-menu");
 
 navElement.addEventListener("click", (event) => {
-    console.log(event.target.dataset.menu);
+    
     const target = event.target;
     const menu = target.dataset.menu;
-
+    if (!menu) {
+        return;
+    } else {
     const element = document.querySelector(`.${menu}`); 
 
     element.scrollIntoView({behavior:"smooth"});
-
-
 }
 
+})
 
 
+
+// contact btn scrolling //
+
+
+
+const contactBtn = document.querySelector(".contact-btn");
+
+contactBtn.addEventListener("click", 
+    () => {
+        const contactMenu = document.querySelector(".contact");
+        contactMenu.scrollIntoView({behavior:"smooth"});
+    }
 
 )
+
+
+// function scrollIntoView(seletor) {
+//     const scrollTo = document.querySelector("selector");
+//     scrollTo.scrollIntoView({behavior:"smooth"});
+// }
+// 펑션 지정해 두고 간단하게 쓰기. 
+
+
+
+// scrolling with fade-out
+
+const homeScreen = document.querySelector(".home");
+const homeScreenHeight = homeScreen.getBoundingClientRect().height;
+const homeOpaityElement = document.querySelector(".home .header")
+
+document.addEventListener("scroll", () => {
+    const opacityValue = 1- window.scrollY/homeScreenHeight;
+    homeOpaityElement.style.opacity = opacityValue;
+})
+
