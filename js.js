c12ff90a4ee8bb85d7824bc80e15d032b924cfab
goddemi <goddemi0124@gmail.com>
2022-06-12 15:18:00 +0900

    menu click contents change

--- js.js
+++ js.js
@@ -17,26 +17,32 @@ navBar.addEventListener("click", (e) => {
 
 //contents
 
-// const likesBox = document.querySelector(".likes_box");
-// const likeContents = document.querySelectorAll(".likes_contents");
-// const newlikeContents = [...likeContents];
+// 1. 누른다.
+// 2. 기존의 것이 없어진다. 클래스에 hidden 이 추가된다.
+// 3. 누른것의 data set을 찾는다. 그 데이터 set이 아닌 것들에 hidden이 추가된다.
+// 4. 그 데이터 set에 속하는 것들에 hidden을 뺀다.
 
-// let menuName;
+const likeMenu = document.querySelector(".likes_box");
+const likeContents = document.querySelectorAll(".likes_contents");
+const arrayLikeContents = [...likeContents];
 
-// function contentClassify(event) {
-//   if (event.target.className !== "likes_box") {
-//     menuName = event.target.innerHTML;
-//     const newnew = newlikeContents.filter((content) => {
-//       content.dataset.group === menuName;
-//     });
-//     newnew.forEach((content) => {
-//       if (content.classList.contains("hidden")) {
-//         content.classList.remove("hidden");
-//       }
-//     });
-//   }
-// }
+function contentDeleting(groupName) {
+  arrayLikeContents.forEach((content) => {
+    if (content.dataset.group !== groupName) {
+      content.classList.add("hidden");
+    } else {
+      content.classList.remove("hidden");
+    }
+  });
+}
 
-// likesBox.addEventListener("click", contentClassify);
+function contentChanging(event) {
+  console.dir(event.target);
+  let groupName;
+  if (event.target.className === "likes_name") {
+    groupName = event.target.innerHTML;
+    contentDeleting(groupName);
+  } else return;
+}
 
-// dataset.group;
+likeMenu.addEventListener("click", contentChanging);
